3ad2118cd6fe2dd74f138c38fc0b16bd715b36ef
gsanchez <gonzalo.sanchezsilva@gmail.com>
2021-03-27 13:47:54 +0100

    fix: env path in helper

--- helper.rb
+++ helper.rb
@@ -3,10 +3,14 @@
 require "oauth2"
 require "dotenv"
 
-config = File.absolute_path(File.join(File.expand_path(".env"), '..', '.env'))
+#config = File.absolute_path(File.join(File.expand_path(".env"), '..', '.env'))
+config = File.join(File.dirname(File.expand_path(__FILE__)), '.env')
 Dotenv.load(config)
 
 def getToken()
+    config = File.join(File.dirname(File.expand_path(__FILE__)), '.env')
+    puts config
+    puts ENV['UID']
     client = OAuth2::Client.new(ENV['UID'], ENV['SECRET'], site: "https://api.intra.42.fr")
     return client.client_credentials.get_token
 end
